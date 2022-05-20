74b02b3bd896199fe95257b188ad2bdf44824fac
Lana Septiana <lana.septiana2@gmail.com>
2022-05-20 13:21:19 +0700

    New: solveMeFirst Challenge

---
+++ Easy/solveMeFirst.js
@@ -0,0 +1,34 @@
+/**
+    Complete the function solveMeFirst to compute the sum of two integers.
+
+    Example
+
+
+    Return .
+
+    Function Description
+
+    Complete the solveMeFirst function in the editor below.
+
+    solveMeFirst has the following parameters:
+
+    int a: the first value
+    int b: the second value
+    Returns
+    - int: the sum of  and 
+
+    Constraints
+
+
+    Sample Input
+
+    a = 2
+    b = 3
+    Sample Output
+
+    5
+ */
+
+function solveMeFirst(a, b) {
+    return a + b;
+}
